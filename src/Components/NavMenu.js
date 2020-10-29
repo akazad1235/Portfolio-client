520f519be06df55b18efea144bd4e866d49a6e64
akazad1235 <akazad914@gmail.com>
2020-10-29 14:37:31 +0600

    Navbar & React Router Added

---
+++ src/Components/NavMenu.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+
+const NavMenu = () => {
+    return (
+        <div className="navbar-bg">
+            <Container>
+                <Navbar  expand="lg" >
+                    <Navbar.Brand href="#home" className="text-white">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end" >
+                        <Nav className="ml-auto m-0 p-0">
+                            <NavLink  to="/home" className="text-white mx-2 d-inline p-2">Home</NavLink>
+                            <NavLink to="/home" className="text-white mx-2 d-inline p-2">Project</NavLink>
+                            <NavLink to="/home" className="text-white mx-2 d-inline p-2">Blog</NavLink>
+                            <NavLink  to="/home" className="text-white mx-2 d-inline p-2">About</NavLink>
+                            <NavLink  to="/home" className="text-white mx-2 d-inline p-2">Contact</NavLink>
+                            <NavLink  to="/home" className="text-white btn-outline-danger border rounded mx-2 resume-button d-inline px-3 p-1">Resume</NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </Container>
+        </div>
+    );
+};
+
+export default NavMenu;
---
+++ src/Pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavMenu from '../Components/NavMenu';
+
+const Homepage = () => {
+    return (
+        <div>
+            <NavMenu/>
+        </div>
+    );
+};
+
+export default Homepage;
